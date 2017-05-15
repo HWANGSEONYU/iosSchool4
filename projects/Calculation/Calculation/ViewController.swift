ebe195e517aa54298009483e85788066ad3fdd3f
Hwang Seonyu <me@celiaui-MacBook-Pro.local>
2017-05-15 20:53:53 +0900

    계산기 v1

--- projects/Calculation/Calculation/ViewController.swift
+++ projects/Calculation/Calculation/ViewController.swift
@@ -10,16 +10,133 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet var displayLB: UILabel!
+
+    var tempFirstNum:Int = 0
+    var tempSecondNum:Int = 0
+    
+    //var tempOperation:String?
+    var tempOperation:String = ""
+    
+    var displayNum:String = ""
+
+    ////////////////View Lifecycle/////////////////
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib. test4455
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+    }
+    
+    ////////////////View Action/////////////////
+    // MARK: - View Action
+    @IBAction func clickNumBtn7(_ sender: UIButton) {
+        displayNum += "7"
+        /*
+        if tempOperation.isEmpty {
+            
+            // 옵셔널!의 의미: 이건 nil이 아니다
+            // 형변환에 실패하면 nil을 반환
+            //self 생략 가능
+            tempFirstNum = Int(displayNum)!
+        } else {
+            tempSecondNum = Int(displayNum)!
+        }
+        
+        self.displayLB.text = displayNum
+        */
+        displayNumLB()
+    }
+    @IBAction func clickNumBtn8(_ sender: UIButton) {
+/*
+        let displayingText:String = self.displayLB.text!
+        self.displayLB.text = displayingText + "8"
+*/
+        displayNum += "8"
+        displayNumLB()
+    }
+    
+    @IBAction func clickNumBtn(_ sender: UIButton) {
+        displayNum += (sender.titleLabel?.text)!
+        displayNumLB()
+    }
+    @IBAction func touchUpInsideOperationBtn(_ sender:UIButton) {
+        switch sender.currentTitle! {
+        case "+":
+            tempOperation = "+"
+        case "-":
+            tempOperation = "-"
+        case "×":
+            tempOperation = "*"
+        case "÷":
+            tempOperation = "/"
+        default:
+            tempOperation = ""
+        }
+        displayNum = ""
+    }
+    
+    @IBAction func touchUpInsideResultBtn(_ sender: UIButton) {
+        var resultValue = 0
+        
+        switch tempOperation {
+        case "+":
+            resultValue = plus(firstNum: tempFirstNum, secondNum: tempSecondNum)
+        case "-":
+            resultValue = minus(firstNum: tempFirstNum, secondNum: tempSecondNum)
+        case "*":
+            resultValue = multiply(firstNum: tempFirstNum, secondNum: tempSecondNum)
+        case "/":
+            resultValue = divide(firstNum: tempFirstNum, secondNum: tempSecondNum)
+        default:
+            resultValue = 0
+        }
+        self.displayLB.text = "\(resultValue)"
+        
+        initTempVal()
+
+    }
+    @IBAction func touchUpInsideACBtn(_ sender: UIButton) {
+        initTempVal()
+        self.displayLB.text = displayNum
     }
 
+    
+    ////////////////Method/////////////////
+    // MARK: - Method
+    func plus(firstNum:Int, secondNum:Int) ->Int {
+        return firstNum + secondNum
+    }
+    func minus(firstNum:Int, secondNum:Int) ->Int {
+        return firstNum - secondNum
+    }
+    func multiply(firstNum:Int, secondNum:Int) ->Int {
+        return firstNum * secondNum
+    }
+    func divide(firstNum:Int, secondNum:Int) ->Int {
+        return firstNum / secondNum
+    }
+    func displayNumLB(){
+        if tempOperation.isEmpty {
+            tempFirstNum = Int(displayNum)!
+        } else {
+            tempSecondNum = Int(displayNum)!
+        }
+        
+        self.displayLB.text = displayNum
+    }
+    func initTempVal(){
+        tempFirstNum = 0
+        tempSecondNum = 0
+        tempOperation = ""
+        
+        displayNum = ""
+    }
 
 }
 
